f8c69f96137dbcbb11e90f58135c40fd88c282eb
Paula Ulate <paulatecr@gmail.com>
2025-07-27 19:16:51 -0600

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_2___Paula_Ulate_Medrano
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_2___Paula_Ulate_Medrano.MvcApplication" Language="C#" %>
